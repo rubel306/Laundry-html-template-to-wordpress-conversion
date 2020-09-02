9f898556cf2dc5232122926e47c1796dd0a4cbfd
RubelHasan <rubelhasan306@gmail.com>
2020-09-02 14:59:43 +0600

    header update

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title><?php bloginfo( 'title' ) ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
